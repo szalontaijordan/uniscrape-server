7bded73ed6992084fab672839debf51c5248f1b7
Szalontai, Jordan <Jordan_Szalontai@epam.com>
2019-01-28 09:21:30 +0100

    fixed amazon exception

--- src/services/amazon/amazon.headless.service.ts
+++ src/services/amazon/amazon.headless.service.ts
@@ -18,40 +18,39 @@ export class AmazonHeadlessService implements OnInit {
         const URL = this.amazonSearchURL
             .replace('#KEYWORDS#', encodeURIComponent(keywords).replace('%20', '+'))
             .replace('#PAGE#', String(page));
-    
-        try {
-            const resultList = await this.puppeteerService.getInformationFromPage(URL, this.extractAmazonResults());
+        const resultList = await this.puppeteerService.getInformationFromPage(URL, this.extractAmazonResults());
 
-            if (!resultList.length) {
-                throw new AmazonEmptyResultsException('The list of the results is empty');
-            }
-    
-            return resultList;    
-        } catch (e) {
-            throw new AmazonDOMChangedException(e);
+        if (!resultList.length) {
+            throw new AmazonEmptyResultsException('The list of the results is empty');
         }
+
+        return resultList;    
     }
     
     private extractAmazonResults() {
-        return () => {
-            return Array.from(document.getElementsByClassName('s-item-container'))
-                .map(result => {
-                    const data = result.querySelectorAll('.a-col-right .a-spacing-none');
-            
-                    if (!data.length) {
-                        return null;
-                    }
-            
-                    const title = data[0].getElementsByTagName('h2')[0]['innerText'];
-                    const url = data[0].getElementsByTagName('a')[0].href;
-                    const category = data[4] ? data[3]['innerText'] : data[2]['innerText'];
-                    const price = data[4] ? data[4]['innerText'].split('\n')[0] : data[3]['innerText'].split('\n')[0];
-                    const image = result.getElementsByClassName('s-access-image')[0].getAttribute('src');
-            
-                    return { title, url, category, price, image };
-                })
-                .filter(book => !!book);
-        } 
+        try {   
+            return () => {
+                return Array.from(document.getElementsByClassName('s-item-container'))
+                    .map(result => {
+                        const data = result.querySelectorAll('.a-col-right .a-spacing-none');
+                
+                        if (!data.length) {
+                            return null;
+                        }
+                
+                        const title = data[0].getElementsByTagName('h2')[0]['innerText'];
+                        const url = data[0].getElementsByTagName('a')[0].href;
+                        const category = data[4] ? data[3]['innerText'] : data[2]['innerText'];
+                        const price = data[4] ? data[4]['innerText'].split('\n')[0] : data[3]['innerText'].split('\n')[0];
+                        const image = result.getElementsByClassName('s-access-image')[0].getAttribute('src');
+                
+                        return { title, url, category, price, image };
+                    })
+                    .filter(book => !!book);
+            } 
+        } catch (e) {
+            throw new AmazonDOMChangedException('The current selectors were not able to find any books');
+        }
     }
     
 }
